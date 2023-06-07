80dd4acec24019fe032975c0ab5cebf942707eb7
DELL <DELL@jk>
2023-06-07 15:08:02 +0530

    1st Commit

---
+++ mavenproject1/src/test/java/com/actitime/testscript/CreateModule.java
@@ -0,0 +1,12 @@
+package com.actitime.testscript;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CreateModule {
+	@Test
+	public void testcreatemodule() {
+		Reporter.log("createmodule",true);
+	}
+
+}
